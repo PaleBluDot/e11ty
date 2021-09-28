5c32e83aa793cf72f744aa074b0e0cc746e4bf8e
Pavel Sanchez <23495830+PaleBluDot@users.noreply.github.com>
2021-09-28 14:47:14 -0400

    feat(styles): add purgecss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,21 @@
 module.exports = {
-	purge: [
-		"./dist/templates/tailwindcss.html"
-	],
+	purge: {
+		enabled: true,
+		mode: "all",
+		preserveHtmlElements: false,
+		content: ["./src/templates/*.html"],
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
-      colors: {
-			  primary: "#f3ef07",
-			  black: "#4f404d",
-			  white: "#4f404d",
-			  grey: "#888888",
-			  transparent: "transparent",
-			  current: "currentColor",
-		  },
+			colors: {
+				primary: "#f3ef07",
+				black: "#4f404d",
+				white: "#4f404d",
+				grey: "#888888",
+				transparent: "transparent",
+				current: "currentColor",
+			},
 		},
 		variants: {
 			extend: {},
